5c14007003b005fef0e3f93920e00bca2ea22c12
Hossam <7ossam.gamal@gmail.com>
2016-09-27 03:20:21 +0200

    Initial commit

---
+++ app/src/main/java/com/hossam/devloper/meplacedesign/ViewPagerAdapter.java
@@ -0,0 +1,66 @@
+package com.hossam.devloper.meplacedesign;
+
+/**
+ * Created by devloper on 9/26/16.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+
+        Fragment fragment=null;
+        if(position==0){
+            fragment = new Tab1();
+        }
+        else if(position==1){
+            fragment=new Tab2();
+        }
+        else if(position==2){
+            fragment=new Tab3();
+        }
+
+        return fragment;
+
+
+
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
